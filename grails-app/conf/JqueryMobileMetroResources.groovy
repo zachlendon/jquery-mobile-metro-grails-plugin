638a6544d2932a5454c4bc0ad864b5915e170e6e
Zach Lendon <zach.lendon@gmail.com>
2012-05-01 00:13:27 -0500

    configured resources module for jquery-mobile-metro

--- grails-app/conf/JqueryMobileMetroResources.groovy
+++ grails-app/conf/JqueryMobileMetroResources.groovy
@@ -2,7 +2,7 @@ modules = {
 
     'jquery-mobile-metro' {
         dependsOn 'jquery-mobile'
-        resource url:[plugin: 'jquery-mobile', dir: 'css', file: 'jquery.mobile-1.1.0.css'], disposition: 'head'
-        resource url:[plugin: 'jquery-mobile', dir: 'js', file: 'jquery.mobile-1.1.0.js'], disposition: 'head'
+        resource url:[plugin: 'jquery-mobile-metro', dir: 'css', file: 'jquery.mobile.metro.theme.css'], disposition: 'head'
+        resource url:[plugin: 'jquery-mobile-metro', dir: 'js', file: 'jquery.mobile.metro.theme.init.js'], disposition: 'head'
     }
 }
