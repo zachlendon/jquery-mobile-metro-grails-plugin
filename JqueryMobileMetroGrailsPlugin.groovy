e623c963752c0c4de3776fc1ba443c0a5e7724d6
Zach Lendon <zach.lendon@gmail.com>
2012-05-01 00:11:02 -0500

    fixed issues with it not being runnable (now starts)

--- JqueryMobileMetroGrailsPlugin.groovy
+++ JqueryMobileMetroGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class JqueryMobileMetroGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = ['resources':'1.0 > *', 'jquery':'1.6 > *']
+    def dependsOn = [:]
 
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
