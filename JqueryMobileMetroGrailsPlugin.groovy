43705e917ad10b110b437b1cd8fd20d0caa1b648
Zach Lendon <zach.lendon@gmail.com>
2012-05-02 23:47:14 -0500

    updated configuration

--- JqueryMobileMetroGrailsPlugin.groovy
+++ JqueryMobileMetroGrailsPlugin.groovy
@@ -13,13 +13,14 @@ class JqueryMobileMetroGrailsPlugin {
 
     def author = "Zach Lendon"
     def authorEmail = "zach.lendon@gmail.com"
-    def title = "Plugin jQuery mobile framework metro theme resource files"
-    def description = 'Plugin jQuery mobile framework metro theme resource files. To get started -- checkout http://jquerymobile.com/'
+    def title = "Plugin containing jQuery Mobile framework Metro UI theme resource files"
+    def description = 'Plugin jQuery mobile framework Metro UI theme resource files.'
 
     // URL to the plugin's documentation
     def documentation = "https://github.com/zachlendon/jquery-mobile-metro-grails-plugin/blob/master/README.md"
     def license = "APACHE"
     def issueManagement = [ system: "github", url: "https://github.com/zachlendon/jquery-mobile-metro-grails-plugin/issues" ]
     def scm = [ url: "https://github.com/zachlendon/jquery-mobile-metro-grails-plugin" ]
-
+    def developers = [
+            [ name: "Zach Lendon", email: "zach.lendon@gmail.com" ]]
 }
