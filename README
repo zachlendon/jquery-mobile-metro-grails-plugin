c8df237a1ff62ad72d4d93128c01c1234d7ee5fa
Zach Lendon <zach.lendon@gmail.com>
2012-05-02 23:18:02 -0500

    upaded resources and configuration settings to reflect recent changes

---
+++ README
@@ -0,0 +1,13 @@
+# JQuery Mobile Metro
+
+This is an experimental Grails project using Metro UI HTML5-based theme on top of JQuery Mobile that mimics Windows Phone 7 native look and feel.  This project is based on the [sgrebnov's JQMobile-Metro-Theme github project](https://github.com/sgrebnov/jqmobile-metro-theme).
+
+## Demo
+
+[Check out the demo](http://jqm-metro-test.cloudfoundry.com) on Cloud Foundry.  
+
+## Installation
+
+You can use the [template project](https://github.com/zachlendon/jquery-mobile-metro-grails-test) as your guide or run:
+
+grails install-plugin jquery-mobile-metro
--- grails-app/conf/JqueryMobileMetroResources.groovy
+++ grails-app/conf/JqueryMobileMetroResources.groovy
@@ -2,7 +2,21 @@ modules = {
 
     'jquery-mobile-metro' {
         dependsOn 'jquery-mobile'
-        resource url:[plugin: 'jquery-mobile-metro', dir: 'css', file: 'jquery.mobile.metro.theme.css'], disposition: 'head'
+        resource url:[plugin: 'jquery-mobile-metro', dir: 'css/metro', file: 'jquery.mobile.metro.theme.css'], disposition: 'head'
+        resource url:[plugin: 'jquery-mobile-metro', dir: 'css', file: 'jquery.mobile.fixedToolbar.polyfill.css'], disposition: 'head'
+        resource url:[plugin: 'jquery-mobile-metro', dir: 'css', file: 'progress-bar.css'], disposition: 'head'
+        resource url:[plugin: 'jquery-mobile-metro', dir: 'css', file: 'app-bar.css'], disposition: 'head'
+        resource url:[plugin: 'jquery-mobile-metro', dir: 'css', file: 'toggle-button.css'], disposition: 'head'
+        resource url:[plugin: 'jquery-mobile-metro', dir: 'css', file: 'mobiscroll-1.5.3.css'], disposition: 'head'
+
         resource url:[plugin: 'jquery-mobile-metro', dir: 'js', file: 'jquery.mobile.metro.theme.init.js'], disposition: 'head'
+        resource url:[plugin: 'jquery-mobile-metro', dir: 'js', file: 'jquery.globalstylesheet.js'], disposition: 'head'
+        resource url:[plugin: 'jquery-mobile-metro', dir: 'js', file: 'jquery.mobile.fixedToolbar.polyfill.js'], disposition: 'head'
+        resource url:[plugin: 'jquery-mobile-metro', dir: 'js', file: 'progress-bar.js'], disposition: 'head'
+        resource url:[plugin: 'jquery-mobile-metro', dir: 'js', file: 'toggle-button.js'], disposition: 'head'
+        resource url:[plugin: 'jquery-mobile-metro', dir: 'js', file: 'app-bar.js'], disposition: 'head'
+        resource url:[plugin: 'jquery-mobile-metro', dir: 'js', file: 'jquery.easing.1.3.js'], disposition: 'head'
+        resource url:[plugin: 'jquery-mobile-metro', dir: 'js', file: 'mobiscroll-1.5.3.js'], disposition: 'head'
     }
+
 }
